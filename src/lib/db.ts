7487e790c6b239a326fda28e6bf980b3e2704463
Valentin Beaussart <93345878+ValentinBeaussart@users.noreply.github.com>
2024-12-10 14:36:38 +0100

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,15 @@
-import { initDb, db } from './db/connection';
+import { initDb, supabase } from './db/connection';
 
 // Initialize the database
 await initDb();
 
-// Re-export everything from the database modules
+// Export everything from the database modules
 export * from './db/types';
 export * from './db/clubs';
 export * from './db/players';
 export * from './db/matches';
 export * from './db/match-players';
+export * from './db/news';
 
 // Export the database connection
-export { db };
+export { supabase };
