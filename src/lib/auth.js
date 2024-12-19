c676080e31e17d4a37db1c87bcee231730a3e1f5
Valentin Beaussart <93345878+ValentinBeaussart@users.noreply.github.com>
2024-12-19 09:56:25 +0100

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,1 +1,12 @@
 
+import { supabase } from './supabase';
+
+export async function protectRoute(Astro) {
+  const { data: { session } } = await supabase.auth.getSession();
+
+  if (!session) {
+    return Astro.redirect('/login');
+  }
+
+  return session;
+}
